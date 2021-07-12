4854f5d3b08f8464815eb6252702422009b2cf80
Flavio Wu <flaviowu@gmail.com>
2021-07-12 20:34:18 -0300

    1st commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function setForm() {
+    let name = document.forms["cadastroForm"]["name"].value;
+    let email = document.forms["cadastroForm"]["email"].value;
+    localStorage.setItem("cadastro", [name, email])
+}
+
+function printaConsole() {
+    console.log(localStorage.getItem("cadastro"))
+}
